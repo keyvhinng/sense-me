710231ac86e02ba216519e3ebc9c6c2d2e2ed598
= <keyvhinng@gmail.com>
2015-11-20 11:32:48 -0500

    client & server for label data

---
+++ server-label-data/public/js/home.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function(){
+
+	$("#btnSave").on('click',function(){
+		console.log("btn was clicked");
+		var tweet = {
+			'id' : 10,
+			'status' : 'new status 0'			
+		};
+		$.ajax({
+			type: 'PUT',
+			data: tweet,
+			dataType: 'JSON', 
+			url : '/tweets/3',
+		}).done(function(response){
+			console.log(response);
+		});
+	});
+});
